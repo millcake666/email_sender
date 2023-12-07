54c147ffcfee2dc7f0b7bc034e42212203d20781
Dmitry Gryaznov <53191191+millcake666@users.noreply.github.com>
2023-12-07 11:33:48 +0500

    wip

--- email_sender.py
+++ email_sender.py
@@ -11,7 +11,7 @@ def send_email(recipient_email: str | list, header_text: str, msg_text: str):
         return
     _cnt = count_send_email_per_day()
     if _cnt + len(recipient_email) > 3:
-        print(f'Лимит на отправку писем в сутки превышен! Сегодня еще можно отправить {3-_cnt} писем.')
+        print(f'Лимит на отправку писем в сутки превышен!')
         return
 
     login = LOGIN
@@ -31,7 +31,7 @@ def send_email(recipient_email: str | list, header_text: str, msg_text: str):
         s.sendmail(login, ', '.join(recipient_email), msg.as_string())
         for _ in range(len(recipient_email)):
             write_log_time_send_email()
-        print('Сообщение было успешно отправлено!')
+        print(f'Сообщение было успешно отправлено! Сегодня еще можно отправить {3 - _cnt - 1} писем.')
     except Exception as _ex:
         print(_ex)
     finally:
